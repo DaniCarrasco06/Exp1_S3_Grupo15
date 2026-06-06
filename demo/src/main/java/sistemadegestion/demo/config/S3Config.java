0e81d29fc6fb2932776d9c414adeddf4cc3d244e
FranAguilar98 <francisca.aguilar.galindo@gmail.com>
2026-06-06 19:38:15 -0400

    Remove unused AWS SDK imports from S3Config.java

--- demo/src/main/java/sistemadegestion/demo/config/S3Config.java
+++ demo/src/main/java/sistemadegestion/demo/config/S3Config.java
@@ -3,9 +3,6 @@ package sistemadegestion.demo.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -21,5 +18,4 @@ public class S3Config {
                 .region(Region.of(region))
                 .build();
     }
-
 }
